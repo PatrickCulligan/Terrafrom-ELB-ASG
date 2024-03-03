e0910e896740589efef4ee083dfe207e2bc0a23d
Patrick Culligan <pjculligan@gmail.com>
2024-03-03 13:44:06 +0000

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,9 +1,7 @@
+sudo apt update
+sudo apt install apache2 -y
 sudo yum update -y
-sudo amazon-linux-extras install docker -y
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-sudo chkconfig docker on
-sudo yum install -y git
-sudo chmod 666 /var/run/docker.sock
-docker pull dhruvin30/dhsoniweb:v1
-docker run -d -p 80:80 dhruvin30/dhsoniweb:latest  
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
+echo "<html><head><title>Welcome</title></head><body><h1>Welcome to My Apache Server on EC2!</h1></body></html>" | sudo tee /var/www/html/index.html
